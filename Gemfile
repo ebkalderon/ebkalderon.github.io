d5bd5e715b1cdd640ce0c0e7ef57f10a21e33241
Eyal Kalderon <ebkalderon@gmail.com>
2017-11-25 02:08:00 -0500

    Replace entire Particle theme with custom code

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org/'
+
+group :jekyll_plugins do
+    gem 'bootstrap', '~> 4.0.0.alpha3'
+    gem 'jekyll-autoprefixer'
+    gem 'jekyll-tidy'
+end
+
+gem 'bourbon'
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,59 @@
-document.addEventListener("DOMContentLoaded",function(){new SweetScroll({});particlesJS("particles-js",{particles:{number:{value:30,density:{enable:!0,value_area:800}},color:{value:"#ffffff"},shape:{type:"polygon",stroke:{width:0,color:"#000000"},polygon:{nb_sides:5},image:{src:"img/github.svg",width:100,height:100}},opacity:{value:.5,random:!1,anim:{enable:!1,speed:1,opacity_min:.1,sync:!1}},size:{value:3,random:!0,anim:{enable:!1,speed:19.18081918081918,size_min:.1,sync:!1}},line_linked:{enable:!0,distance:150,color:"#ffffff",opacity:.4,width:1},move:{enable:!0,speed:4,direction:"none",random:!0,straight:!1,out_mode:"out",bounce:!1,attract:{enable:!1,rotateX:600,rotateY:1200}},nb:80},interactivity:{detect_on:"window",events:{onhover:{enable:!1,mode:"grab"},onclick:{enable:!0,mode:"push"},resize:!0},modes:{grab:{distance:400,line_linked:{opacity:1}},bubble:{distance:400,size:40,duration:2,opacity:8,speed:3},repulse:{distance:200,duration:.4},push:{particles_nb:4},remove:{particles_nb:2}}},retina_detect:!0})},!1);
+jQuery(document).ready(function($) {
+	//toggle 3d navigation
+	$('.navbar-buttons-trigger').on('click', function() {
+        var marker = $('.navbar-marker');
+        var selectedItem = $('.navbar-buttons-selected');
+        marker.width(selectedItem.width());
+		toggle3dBlock(!$('.navbar-header').hasClass('nav-visible'));
+	});
+
+	//select a new item from the 3d navigation
+	$('.navbar-buttons').on('click', 'a', function() {
+		var selected = $(this);
+		selected.parent('li').addClass('navbar-buttons-selected').siblings('li').removeClass('navbar-buttons-selected');
+		updateSelectedNav('close');
+	});
+
+	$(window).on('resize', function() {
+		window.requestAnimationFrame(updateSelectedNav);
+	});
+
+	function toggle3dBlock(addOrRemove) {
+		if (typeof(addOrRemove)==='undefined') addOrRemove = true;	
+		$('.navbar-header').toggleClass('nav-visible', addOrRemove);
+		$('.navbar-container').toggleClass('nav-visible', addOrRemove);
+		$('main').toggleClass('nav-visible', addOrRemove).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
+			//fix marker position when opening the menu (after a window resize)
+			addOrRemove && updateSelectedNav();
+		});
+	}
+
+	//this function update the .cd-marker position
+	function updateSelectedNav(type) {
+		var selectedItem = $('.navbar-buttons-selected'),
+			selectedItemPosition = selectedItem.index() + 1, 
+			leftPosition = selectedItem.offset().left,
+			backgroundColor = selectedItem.data('navbar-color'),
+			marker = $('.navbar-marker');
+
+        marker.width(selectedItem.width());
+		marker.removeClassPrefix('navbar-color').addClass('navbar-color-'+ selectedItemPosition).css({
+			'left': leftPosition,
+		});
+		if (type == 'close') {
+			marker.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				toggle3dBlock(false);
+			});
+		}
+	}
+
+	$.fn.removeClassPrefix = function(prefix) {
+	    this.each(function(i, el) {
+	        var classes = el.className.split(" ").filter(function(c) {
+	            return c.lastIndexOf(prefix, 0) !== 0;
+	        });
+	        el.className = $.trim(classes.join(" "));
+	    });
+	    return this;
+	};
+});
